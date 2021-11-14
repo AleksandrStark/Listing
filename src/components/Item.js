1abb8527c3b7246c992e5d6f8bbfd932fed515ba
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2021-11-14 20:42:07 +0300

    first commit

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+export default function Item({item}) {
+  return (   
+      <div className="item">
+          <div className="item-image">
+              <a href={item.url}>
+                  <img src={item.MainImage && item.MainImage.url_570xN} alt="card"/>
+              </a>
+          </div>
+          <div className="item-details">
+              <p className="item-title">{item.title}</p>
+              <p className="item-price">{item.price}</p>
+              <p className={`item-quantity ${item.quantity}`}>{item.quantity} left</p>
+          </div>
+      </div>
+  ) 
+}
